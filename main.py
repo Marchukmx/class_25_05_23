9621791ae38432b524d735068d3778c1465963e3
marchukmx <mmmarch1k@gmail.com>
2023-05-25 19:08:46 +0300

    че-то

--- main.py
+++ main.py
@@ -3,3 +3,13 @@ import urllib.request
 opener = urllib.request.build_opener()
 response = opener.open("https://httpbin.org/get")
 print(response.read())
+
+
+import requests
+
+response = requests.get("https://coinmarketcap.com/get")
+print(response.text)
+response_parse = response.text.split('<span>')
+for elem in response_parse:
+    if elem.startswith('$'):
+        print(elem)
